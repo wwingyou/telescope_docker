3660f37aac63956e2adf418394a66dcb3da3adc6
wingyou <winwing.you@gmail.com>
2025-03-06 17:27:27 +0900

    Add 'show docker containers' feature

--- lua/telescope-docker/init.lua
+++ lua/telescope-docker/init.lua
@@ -52,17 +52,13 @@ M.view_docker_images = function (opts)
         local selection = action_state.get_selected_entry()
         actions.close(prompt_bufnr)
 
-        log.debug("selected", selection)
-
         local command = {
-          "edit",
-          "term://docker",
+          "!docker",
           "run",
-          "-it",
+          "-d",
           selection.value.Repository
         }
 
-        log.debug("command", command)
         vim.cmd(vim.fn.join(command, ' '))
       end)
       return true
@@ -70,6 +66,65 @@ M.view_docker_images = function (opts)
   }):find()
 end
 
-M.view_docker_images()
+M.view_docker_containers = function (opts)
+  pickers.new(opts, {
+    finder = finders.new_async_job({
+      command_generator = function ()
+        return {'docker', 'container', 'ls', '--format', 'json'}
+      end,
+      entry_maker = function (entry)
+        local parsed = vim.json.decode(entry)
+        if parsed then
+          return {
+            value = parsed,
+            display = parsed.Names,
+            ordinal = parsed.Names .. parsed.Image,
+          }
+        end
+      end
+    }),
+    sorter = config.generic_sorter(opts),
+    previewer = previewers.new_buffer_previewer ({
+      title = "Docker Container Details",
+      define_preview = function (self, entry)
+        vim.api.nvim_buf_set_lines(
+          self.state.bufnr,
+          0,
+          0,
+          true,
+          vim.tbl_flatten({
+            '```lua',
+            vim.split(vim.inspect(entry.value), '\n'),
+            '```'
+          })
+        )
+        utils.highlighter(self.state.bufnr, "markdown")
+      end
+    }),
+
+    attach_mappings = function (prompt_bufnr)
+      actions.select_default:replace(function ()
+        local selection = action_state.get_selected_entry()
+        actions.close(prompt_bufnr)
+
+        local command = {
+          "edit",
+          "term://docker",
+          "exec",
+          "-it",
+          selection.value.ID,
+          "sh"
+        }
+
+        vim.cmd(vim.fn.join(command, ' '))
+      end)
+      return true
+    end
+  }):find()
+end
+
+
+-- M.view_docker_images()
+-- M.view_docker_containers()
 
 return M
